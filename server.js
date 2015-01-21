7de14a98e558f5ec9ae0f52c02041352033e77cc
Tobi Knaup <tobi@knaup.me>
2015-01-21 20:25:58 +0000

    Add hostname header

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     morgan = require('morgan'),
     redis = require("redis"),
+    os = require("os"),
     app = express(),
     client = redis.createClient(6379, process.env.REDIS_HOST);
 
@@ -9,6 +10,11 @@ client.on("error", function (err) {
 });
 
 app.use(morgan('combined'));
+
+app.use(function(req, res, next) {
+    res.header('X-Hostname', os.hostname());
+    next();
+});
 
 app.get('/', function(req, res) {
     client.get('value', function (error, value) {
